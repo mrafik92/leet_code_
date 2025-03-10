0ffd11ad2a261fa4d4843f3762bbd3d8cfef5545
Rafik E <mrafik3@outlook.com>
2025-03-10 13:20:25 +0100

    sliding window with at most k - at most k + 1

--- src/main.rs
+++ src/main.rs
@@ -102,8 +102,9 @@ impl Solution {
         if g <= 1 {
             return vec![];
         }
-        
+
         let g_usize = g as usize;
+        // using the sieve of eratosthenes
         let mut primes = vec![];
         let mut is_prime = vec![true; g_usize + 1];
         is_prime[0] = false;
@@ -111,9 +112,10 @@ impl Solution {
 
         // Only need to check up to sqrt(g)
         let sqrt_g = (g as f64).sqrt() as usize;
-        
+
         for i in 2..=sqrt_g {
-            if is_prime[i] {                // Mark all multiples as non-prime
+            if is_prime[i] {
+                // Mark all multiples as non-prime
                 let mut j = i * i;
                 while j <= g_usize {
                     is_prime[j] = false;
@@ -127,7 +129,6 @@ impl Solution {
                 primes.push(i as i32);
             }
         }
-
 
         primes
     }
@@ -149,6 +150,48 @@ impl Solution {
             return vec![-1, -1];
         }
         vec![primes[diff.0], primes[diff.0 + 1]]
+    }
+
+    pub fn count_of_super_constrained(word: String, k: i32) -> i64 {
+        // sliding window with at most k constants - at most k + 1 constants
+        let chars= word.chars().collect::<Vec<char>>();
+        Self::count_of_constrained_substrings(&chars, k) - Self::count_of_constrained_substrings(&chars, k + 1)
+    }
+
+    pub fn count_of_constrained_substrings(chars: &[char], k: i32) -> i64 {
+        let mut left = 0;
+        let mut right = 0;
+        let mut constants = 0;
+        let mut substrings = 0;
+
+        let mut vowels_map = HashMap::new();
+
+        while right < chars.len() {
+            if "aeiou".contains(chars[right]) {
+                vowels_map.insert(chars[right], 1 + vowels_map.get(&chars[right]).unwrap_or(&0));
+            } else {
+                constants += 1;
+            }
+
+            while vowels_map.len() == 5 && constants >= k {
+                substrings += (chars.len() - right) as i64;
+
+                if "aeiou".contains(chars[left]) {
+                    let count = vowels_map.get_mut(&chars[left]).unwrap();
+                    *count -= 1;
+                    if *count == 0 {
+                        vowels_map.remove(&chars[left]);
+                    }
+                } else {
+                    constants -= 1;
+                }
+
+                left += 1;
+            }
+            right += 1;
+        }
+
+        substrings
     }
 }
 
@@ -238,10 +281,42 @@ mod tests {
         assert_eq!(Solution::closest_primes(1, 1000000), vec![2, 3]);
         assert_eq!(Solution::closest_primes(1000, 5000), vec![1019, 1021]);
         assert_eq!(Solution::closest_primes(20000, 30000), vec![20021, 20023]);
-        assert_eq!(Solution::closest_primes(100000, 200000), vec![100151, 100153]);
-        assert_eq!(Solution::closest_primes(999000, 1000000), vec![999329, 999331]);
-        
+        assert_eq!(
+            Solution::closest_primes(100000, 200000),
+            vec![100151, 100153]
+        );
+        assert_eq!(
+            Solution::closest_primes(999000, 1000000),
+            vec![999329, 999331]
+        );
+    }
 
-        
+    #[test]
+    fn test_count_of_constrained_substrings() {
+
+        assert_eq!(
+            Solution::count_of_super_constrained("aoaiuefi".to_string(), 1),
+            4
+        );
+
+
+        assert_eq!(
+            Solution::count_of_super_constrained("aadieuoh".to_string(), 1),
+            2
+        );
+
+        assert_eq!(
+            Solution::count_of_super_constrained("iqeaouqi".to_string(), 2),
+            3
+        );
+
+        assert_eq!(
+            Solution::count_of_super_constrained("ieaouqqieaouqq".to_string(), 1),
+            3
+        );
+        assert_eq!(
+            Solution::count_of_super_constrained("buoeia".to_string(), 0),
+            1
+        );
     }
 }
