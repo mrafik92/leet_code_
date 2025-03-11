b7c2bdac2ad1c431f20486c9743af00873ccbe86
Rafik <mrafik3@outlook.com>
2025-03-11 14:49:20 +0100

    Add definitions for some problems

--- src/solution/count_of_every_minimum_substring.rs
+++ src/solution/count_of_every_minimum_substring.rs
@@ -1,3 +1,41 @@
+/*
+LeetCode Problem 1460: Number of Substrings Containing All Three Characters
+Link: https://leetcode.com/problems/number-of-substrings-containing-all-three-characters
+Difficulty: Medium
+
+Description:
+Given a string s consisting only of characters a, b and c.
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+ 
+Example 1:
+
+Input: s = "abcabc"
+Output: 10
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+
+Example 2:
+
+Input: s = "aaacb"
+Output: 3
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+
+Example 3:
+
+Input: s = "abc"
+Output: 1
+
+ 
+Constraints:
+
+3 <= s.length <= 5 x 10^4
+s only consists of a, b or c characters.
+
+Examples:
+"abcabc"
+"aaacb"
+"abc"
+*/
+
 pub fn count_of_every_minimum_substring(s: String) -> i32 {
   let mut left = 0;
   let mut map = [0; 3];
